b489243bd4105205927c046b84b150a1e34981a0
Joe Huss <detain@interserver.net>
2024-03-18 13:43:35 -0400

    updates to _get_password calls adding optional custid parameter for a big speed improvement

--- src/Plugin.php
+++ src/Plugin.php
@@ -57,7 +57,7 @@ class Plugin
             if (trim($hostname) == '') {
                 $hostname = $serviceClass->getId().'.server.com';
             }
-            $password = website_get_password($serviceClass->getId());
+            $password = website_get_password($serviceClass->getId(), $serviceClass->getCustid());
             $username = get_new_webhosting_username($serviceClass->getId(), $hostname, $serviceClass->getServer());
             $data = $GLOBALS['tf']->accounts->read($serviceClass->getCustid());
             [$user, $pass] = explode(':', $hash);
